97b42a018a23043da86c7877ae07e5bb97e005ed
Grigory Bordyugov <gregbg@gmail.com>
2021-01-30 13:28:01 +0100

    joining both tables on customer_id

--- pndapetzim/data.py
+++ pndapetzim/data.py
@@ -2,9 +2,18 @@ from pandas import read_csv
 from pandas import to_datetime
 
 
+FROM_DATE = to_datetime('2015-03-01')
+TO_DATE = to_datetime('2017-02-28')
+
+ORDER_FILE_NAME = 'machine_learning_challenge_order_data.csv.gz'
+LABEL_FILE_NAME = 'machine_learning_challenge_labeled_data.csv.gz'
+
 def read_order_table(
-    path='data/machine_learning_challenge_order_data.csv.gz', *vargs, **kwargs
+    path='data/' + ORDER_FILE_NAME, *vargs, **kwargs
 ):
+    """Read order table, parsing customer ids as hexadecimal integers
+    and turning order dates into 64bit timestamps."""
+
     customer_id_key = 'customer_id'
     order_date_key = 'order_date'
 
@@ -22,3 +31,36 @@ def read_order_table(
     df[order_date_key] = df[order_date_key].apply(lambda d: dates_lut[d])
 
     return df
+
+
+def read_label_table(
+    path='data/' + LABEL_FILE_NAME, *vargs, **kwargs
+):
+    """Load label table. Parse customer ids as hexadecimal
+    integers."""
+
+    customer_id_key = 'customer_id'
+
+    # Parse customer id as a hexadecimal int.
+    def customer_id_converter(customer_id):
+        return int(customer_id, 16)
+
+    converters = {customer_id_key: customer_id_converter}
+
+    return read_csv(path, converters=converters, *vargs, **kwargs)
+
+
+def get_labeled_data(
+    order_path='data/' + ORDER_FILE_NAME,
+    label_path='data/' + LABEL_FILE_NAME,
+):
+    """Load order table and label table and join them on customer_id
+    field."""
+    customer_id_key = 'customer_id'
+
+    orders = read_order_table(order_path)
+    labels = read_label_table(label_path)
+
+    return orders.join(
+        labels.set_index(customer_id_key), on=customer_id_key, how='inner'
+    )
